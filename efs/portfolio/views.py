7570c40c91414d93fcfeac3f46efa78f2345ebc1
bhuvi <bhuvionline@live.com>
2020-11-16 22:07:03 -0600

    EFS commit2

---
+++ efs/portfolio/views.py
@@ -0,0 +1,177 @@
+from django.contrib import messages
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import *
+from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import *
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect
+
+
+now = timezone.now()
+
+# def my_password_change(request):
+#     return redirect('/home')
+
+
+def home(request):
+    return render(request, 'portfolio/home.html',
+                  {'portfolio': home})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/home')
+#
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                #logger.info("123")
+                return redirect('/home')
+
+
+    form = AuthenticationForm(request)
+    return render(request, 'registration/login.html', {'form': form})
+
+
+@login_required
+def customer_list(request):
+    customers = Customer.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'portfolio/customer_list.html',
+                  {'customers': customers})
+
+
+@login_required
+def customer_edit(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == "POST":
+        # update
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.updated_date = timezone.now()
+            customer.save()
+            customer = Customer.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'portfolio/customer_list.html',
+                          {'customers': customer})
+    else:
+        # edit
+        form = CustomerForm(instance=customer)
+    return render(request, 'portfolio/customer_edit.html', {'form': form})
+
+
+@login_required
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customer.delete()
+    return redirect('portfolio:customer_list')
+
+
+@login_required
+def stock_list(request):
+    stocks = Stock.objects.filter(purchase_date__lte=timezone.now())
+    return render(request, 'portfolio/stock_list.html', {'stocks': stocks})
+
+
+@login_required
+def stock_new(request):
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.is_valid():
+            stock = form.save(commit=False)
+            stock.created_date = timezone.now()
+            stock.save()
+            stocks = Stock.objects.filter(purchase_date__lte=timezone.now())
+            return render(request, 'portfolio/stock_list.html',
+                          {'stocks': stocks})
+    else:
+        form = StockForm()
+        # print("Else")
+    return render(request, 'portfolio/stock_new.html', {'form': form})
+
+
+@login_required
+def stock_edit(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    if request.method == "POST":
+        form = StockForm(request.POST, instance=stock)
+        if form.is_valid():
+            stock = form.save()
+            # stock.customer = stock.id
+            stock.updated_date = timezone.now()
+            stock.save()
+            stocks = Stock.objects.filter(purchase_date__lte=timezone.now())
+            return render(request, 'portfolio/stock_list.html', {'stocks': stocks})
+    else:
+        # print("else")
+        form = StockForm(instance=stock)
+    return render(request, 'portfolio/stock_edit.html', {'form': form})
+
+
+@login_required
+def stock_delete(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    stock.delete()
+    return redirect('portfolio:stock_list')
+
+
+@login_required
+def investment_list(request):
+    investments = Investment.objects.filter(acquired_date__lte=timezone.now())
+    return render(request, 'portfolio/investment_list.html', {'investments': investments})
+
+
+@login_required
+def investment_new(request):
+    if request.method == "POST":
+        form = InvestmentForm(request.POST)
+        if form.is_valid():
+            investment = form.save(commit=False)
+            investment.acquired_date = timezone.now()
+            investment.save()
+            investments = Investment.objects.filter(acquired_date__lte=timezone.now())
+            return render(request, 'portfolio/investment_list.html',
+                          {'investments': investments})
+    else:
+        form = InvestmentForm()
+        # print("Else")
+    return render(request, 'portfolio/investment_new.html', {'form': form})
+
+
+@login_required
+def investment_edit(request, pk):
+    investment = get_object_or_404(Investment, pk=pk)
+    if request.method == "POST":
+        form = InvestmentForm(request.POST, instance=investment)
+        if form.is_valid():
+            investment = form.save()
+            # stock.customer = stock.id
+            investment.updated = timezone.now()
+            investment.save()
+            investments = Investment.objects.filter(acquired_date__lte=timezone.now())
+            return render(request, 'portfolio/investment_list.html', {'investments': investments})
+    else:
+        # print("else")
+        form = InvestmentForm(instance=investment)
+        return render(request, 'portfolio/investment_edit.html', {'form': form})
+
+
+@login_required
+def investment_delete(request, pk):
+    investment = get_object_or_404(Investment, pk=pk)
+    investment.delete()
+    return redirect('portfolio:investment_list')
