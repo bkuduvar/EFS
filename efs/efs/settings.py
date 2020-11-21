2de86211a7f1086579f84cf08502db8a96176ebd
bhuvi <bhuvionline@live.com>
2020-11-20 18:21:09 -0600

    Update settings.py

--- efs/efs/settings.py
+++ efs/efs/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
